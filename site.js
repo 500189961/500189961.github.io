10be3eed8fcd0e92334d75fbdce69fe34ca6d53d
500189961 <500189961@fvtc.edu>
2024-02-10 19:13:21 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -10,3 +10,20 @@ contactLink.style.fontWeight = "bold"
 
 const links = document.querySelectorAll('nav a')
 links.forEach(link => link.style.display = 'bold')
+
+
+const name = 'Anna'
+const age = 21
+
+console.log(name + " is " + age + " years old")
+
+
+const message = document.getElementById("message")
+const hours = new Date().getHours()
+let timeOfDay =""
+
+if  (hours >=4 & hours <12) timeOfDay = "It is Morning"
+else if (hours >= 12 && hours >17) timeOfDay = "It is Afternoon"
+else if (hours >= 17 || hours <4) timeOfDay = "It is Evening"
+
+message.innerHTML = timeOfDay
